857b6f32376ab1f79076e0e12d3885d2c9b5bc97
CVCurcurato <christiancurcurato@gmail.com>
2025-06-03 22:26:06 -0400

    Update SQL Challenge.sql

--- SQL Challenge.sql
+++ SQL Challenge.sql
@@ -1,22 +1,43 @@
 -- All queries are written in T-SQL syntax
 
 -- Query 1
--- The CUST_ACCT table records relationships between customers and the accounts they own. The table contains a column named CUST_ROLE which has two distinct values: 'primary', 'secondary'. Each account has one, and only one, primary owner but can have zero, one, or many secondary owners. 
+-- The CUST_ACCT table records relationships between customers and the accounts they own. The table contains a column named CUST_ROLE which has two distinct values: 'primary', 'secondary'. 
+-- Each account has one, and only one, primary owner but can have zero, one, or many secondary owners. 
 -- Please write a query that will return the Account ID and Account Title for all accounts opened over the past 6 full calendar months where the age of the primary customer associated with the account was 30 years or younger at the time of account opening.
 
---Create CTE of all customer and account information for the past 6 months (including duplicate columns like cust_id and acct_id)
+--Create CTE of all customer and account information for the past 6 months
 WITH past_6_months AS (
-SELECT *
+SELECT
+  C.cust_id,
+  C.cust_name,
+  C.cust_birth_date,
+  CA.cust_role,
+  A.acct_id,
+  A.acct_open_date,
+  A.acct_title
 FROM customers AS C
 INNER JOIN cust_acct AS CA
   ON C.cust_id = CA.cust_id
 INNER JOIN accounts AS A
     ON CA.acct_id = A.acct_id
-WHERE DATEDIFF(month, acct_open_date, TODAY()) = 6
+WHERE A.acct_open_date >= DATEADD(month, -6, GETDATE())
 )
 
 -- Filter for primary customers 30 years or younger at time of account opening
-SELECT CA.acct_id, acct_title
+SELECT 
+  acct_id, 
+  acct_title
 FROM past_6_months
-WHERE DATEDIFF(year, cust_birth_date, acct_open_date) <= 30
-AND cust_role = 'primary'
+WHERE cust_role = 'primary'
+AND DATEDIFF(year, cust_birth_date, acct_open_date) - 
+    CASE 
+        WHEN MONTH(acct_open_date) < MONTH(cust_birth_date) 
+             OR (MONTH(acct_open_date) = MONTH(cust_birth_date) AND DAY(acct_open_date) < DAY(cust_birth_date))
+        THEN 1 ELSE 0 
+    END <= 30
+;
+
+-- Query 2
+-- Please write a query that will return the Customer ID and Name of all customers who are both primary and secondary owners of accounts. 
+-- If a customer is only the primary owner of accounts, the customer should not be included in the results. 
+-- Likewise, if a customer is only a secondary owner of accounts, the customer should not be included in the results.
