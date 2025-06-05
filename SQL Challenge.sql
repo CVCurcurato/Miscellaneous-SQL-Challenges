5d3d7c23e955517a4a5e6cb207112cc7e8944c17
CVCurcurato <christiancurcurato@gmail.com>
2025-06-04 21:20:23 -0400

    Update SQL Challenge.sql

--- SQL Challenge.sql
+++ SQL Challenge.sql
@@ -1,11 +1,8 @@
 -- All queries are written in T-SQL syntax
 
--- Query 1
--- The CUST_ACCT table records relationships between customers and the accounts they own. The table contains a column named CUST_ROLE which has two distinct values: 'primary', 'secondary'. 
--- Each account has one, and only one, primary owner but can have zero, one, or many secondary owners. 
--- Please write a query that will return the Account ID and Account Title for all accounts opened over the past 6 full calendar months where the age of the primary customer associated with the account was 30 years or younger at the time of account opening.
+-- Query 1 Solution
 
---Create CTE of all customer and account information for the past 6 months
+--Create CTE of all customer and account information for the past 6 full months
 WITH past_6_months AS (
 SELECT
   C.cust_id,
@@ -20,7 +17,9 @@ INNER JOIN cust_acct AS CA
   ON C.cust_id = CA.cust_id
 INNER JOIN accounts AS A
     ON CA.acct_id = A.acct_id
-WHERE A.acct_open_date >= DATEADD(month, -6, GETDATE())
+-- WHERE statement filters for first day of the month 6 months ago and first day of this month to give past 6 full months
+WHERE A.acct_open_date >= DATEADD(day, 1 - DAY(DATEADD(month, -6, GETDATE())), DATEADD(month, -6, GETDATE()))
+  AND A.acct_open_date < DATEADD(day, 1 - DAY(GETDATE()), GETDATE())
 )
 
 -- Filter for primary customers 30 years or younger at time of account opening
@@ -37,15 +36,21 @@ AND DATEDIFF(year, cust_birth_date, acct_open_date) -
     END <= 30
 ;
 
--- Query 2
--- Please write a query that will return the Customer ID and Name of all customers who are both primary and secondary owners of accounts. 
--- If a customer is only the primary owner of accounts, the customer should not be included in the results. 
--- Likewise, if a customer is only a secondary owner of accounts, the customer should not be included in the results.
+-- Query 2 Solution
 
+-- Select ids and names only for those customers that are primary owners of 1 or more accounts AND secondary owners of 1 or more accounts.
+SELECT 
+  C.cust_id, 
+  C.cust_name
+FROM customers AS C
+INNER JOIN cust_acct AS CA
+  ON C.cust_id = CA.cust_id
+GROUP BY C.cust_id, C.cust_name
+HAVING COUNT(DISTINCT CASE WHEN CA.cust_role = 'primary' THEN CA.acct_id END) >= 1
+   AND COUNT(DISTINCT CASE WHEN CA.cust_role = 'secondary' THEN CA.acct_id END) >= 1
+;
 
-
--- Query 3
--- Please write a query that will return the State Code, Account ID, Account Title, and Account Balance for the largest account (measured by Account Balance) in each state.
+-- Query 3 Solution
 
 -- Find largest account per state
 WITH max_per_state AS (
@@ -66,4 +71,20 @@ FROM accounts AS A
 INNER JOIN max_per_state AS M
   ON A.state_code = M.state_code
   AND acct_balance = largest_acct
+;
+
+-- Query 4 Solution
+
+-- Select Account ID & Title, then identify when an attribute matches 1,3 and 5 and take those values in respective columns. Null if not present.
+SELECT 
+    A.acct_id,
+    A.acct_title,
+    MAX(CASE WHEN AA.attr_name = 'ATTR_01' THEN AA.attr_value END) AS ATTR_01,
+    MAX(CASE WHEN AA.attr_name = 'ATTR_03' THEN AA.attr_value END) AS ATTR_03,
+    MAX(CASE WHEN AA.attr_name = 'ATTR_05' THEN AA.attr_value END) AS ATTR_05
+FROM accounts A
+LEFT JOIN account_attributes AA 
+    ON A.acct_id = AA.acct_id 
+    AND AA.attr_name IN ('ATTR_01', 'ATTR_03', 'ATTR_05')
+GROUP BY A.acct_id, A.acct_title
 ;
